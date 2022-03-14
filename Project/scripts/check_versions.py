c8f5a271a77a4d0451b256ede437d5955966f670
Eli Smith <googolplex8@yahoo.com>
2022-03-14 14:41:55 -0400

    initial commit

---
+++ Project/scripts/check_versions.py
@@ -0,0 +1,12 @@
+#import tensorflow.compat.v2 as tf
+import tensorflow as tf
+
+#import tensorflow.keras as keras
+
+def main():
+  print(tf.version)
+  #print(keras.__version__)
+
+
+if __name__ == '__main__':
+  main()
